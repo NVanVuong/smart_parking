7c2cd79a819c9ed2603532ebad72809afa20be6e
Hoanqnqu <nhathoangthuychaub1@gmail.com>
2023-03-10 11:10:21 +0700

    Tao header search

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <ul>
+                <li>SIGN IN</li>
+                <li>SIGN UP</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
