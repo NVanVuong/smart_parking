de3a6f6fbdfa6f58b4857e312f205654d5de1249
Nguyễn Văn Vương <93960884+NVanVuong@users.noreply.github.com>
2023-04-26 17:54:57 +0700

    Update UI when availableSpot equals 0

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,25 +2,16 @@ import L from 'leaflet';
 import 'leaflet.awesome-markers/dist/leaflet.awesome-markers';
 L.AwesomeMarkers.Icon.prototype.options.prefix = 'fa';
 
-export const icon = L.icon({
-    iconSize: [40, 40],
-    iconAnchor: [10, 41],
-    popupAnchor: [8, -40],
-    iconUrl: 'https://dbmgns9xjyk0b.cloudfront.net/partner-images/production/bestparking/bp-logo-mobile.png',
-    shadowUrl: 'https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png',
-});
-
-export const iconHere = L.icon({
-    iconSize: [25, 41],
-    iconAnchor: [10, 41],
-    popupAnchor: [2, -40],
-    iconUrl: 'https://unpkg.com/leaflet@1.6/dist/images/marker-icon.png',
-    shadowUrl: 'https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png',
-});
-
 export const parkingSiteMarker = L.AwesomeMarkers.icon({
     icon: 'car',
     markerColor: 'blue',
+    className: 'awesome-marker',
+    shadowUrl: 'https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png',
+});
+
+export const soldOutMarker = L.AwesomeMarkers.icon({
+    icon: 'ban',
+    markerColor: 'lightgray',
     className: 'awesome-marker',
     shadowUrl: 'https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png',
 });
--- src/components/ParkingSiteMarker.js
+++ src/components/ParkingSiteMarker.js
@@ -1,5 +1,5 @@
 import { Marker, Popup } from 'react-leaflet';
-import { parkingSiteMarker } from '../assets/icons/index';
+import { parkingSiteMarker, soldOutMarker } from '../assets/icons/index';
 
 function ParkingSiteMarker({ setShowModal, toggle, setToggle, setCurrentFilter, parkingSite, setSelectedParkingSite }) {
     const handleDetailClick = (parkingSite) => {
@@ -13,23 +13,42 @@ function ParkingSiteMarker({ setShowModal, toggle, setToggle, setCurrentFilter, 
         setSelectedParkingSite(parkingSite);
     };
     return (
-        <Marker key={parkingSite._id} position={parkingSite.position} icon={parkingSiteMarker}>
+        <Marker
+            key={parkingSite._id}
+            position={parkingSite.position}
+            icon={parkingSite.availableSpot > 0 ? parkingSiteMarker : soldOutMarker}
+        >
             <Popup>
                 <div>
                     <div className="mb-2 flex w-52 items-center justify-between">
                         <div className="w-3/5 text-base font-semibold">{parkingSite.name}</div>
-                        <div className="flex items-start text-2xl font-bold text-blue-main">
+                        <div
+                            className={`${
+                                parkingSite.availableSpot > 0 ? 'text-blue-main' : 'text-gray-400'
+                            } flex items-start text-2xl font-bold`}
+                        >
                             {parkingSite.price}
                             <span className="ml-0.5 text-sm">đ</span>
                         </div>
                     </div>
                     <div className="mb-3 whitespace-nowrap text-sm font-medium text-gray-500">
-                        Available: <span className="font-semibold text-gray-800">{parkingSite.availableSpot}</span>
+                        {parkingSite.availableSpot > 0 ? (
+                            <>
+                                Available:{' '}
+                                <span className="font-semibold text-gray-800">{parkingSite.availableSpot}</span>
+                            </>
+                        ) : (
+                            <span className="rounded-md bg-red-600 bg-opacity-80 py-1 px-2 text-xs font-semibold text-white">
+                                Sold out
+                            </span>
+                        )}
                     </div>
                     <div className="flex justify-between space-x-4">
                         <button
                             onClick={() => handleBookClick(parkingSite)}
-                            className="rounded-sm bg-blue-main py-2 px-6 font-semibold text-white shadow-md hover:bg-blue-main-hover hover:ring-4 hover:ring-blue-main-ring active:ring-blue-main-ring"
+                            className={`${
+                                parkingSite.availableSpot > 0 ? 'bg-blue-main' : 'pointer-events-none bg-gray-400'
+                            } rounded-sm  py-2 px-6 font-semibold text-white shadow-md hover:bg-blue-main-hover hover:ring-4 hover:ring-blue-main-ring active:ring-blue-main-ring`}
                         >
                             Book now
                         </button>
